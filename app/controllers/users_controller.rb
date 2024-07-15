9f22d765e72d1f26c9828d480f95f4b943a89795
Brandon Varner <84243251+brvarner@users.noreply.github.com>
2024-07-15 20:12:49 +0000

    prevented users from accessing other people's feeds and discover pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,21 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show liked feed followers following discover ]
+  before_action :set_user, only: %i[show liked followers following discover feed]
+  before_action :correct_user_feed, only: %i[feed]
+  before_action :correct_user_discover, only: %i[discover]
+
 
   private
 
-    def set_user
-      if params[:username]
-        @user = User.find_by!(username: params.fetch(:username))
-      else
-        @user = current_user
-      end
-    end
+  def set_user
+    @user = User.find_by!(username: params[:username]) if params[:username]
+  end
+
+  def correct_user_feed
+    redirect_to feed_path(current_user.username) unless current_user == @user
+  end
+
+  
+  def correct_user_discover
+    redirect_to discover_path(current_user.username) unless current_user == @user
+  end
 end
